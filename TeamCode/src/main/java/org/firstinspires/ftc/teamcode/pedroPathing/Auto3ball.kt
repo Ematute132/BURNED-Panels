6b4626319a8192b6f6f73d52ed7b1b49861ba220
Ematute3 <141678140+Ematute3@users.noreply.github.com>
2025-12-05 16:50:03 -0800

    ddd

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Auto3ball.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Auto3ball.kt
@@ -4,7 +4,6 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous
 import dev.nextftc.core.commands.Command
 import dev.nextftc.core.commands.delays.Delay
 import dev.nextftc.core.commands.groups.ParallelGroup
-import dev.nextftc.core.commands.groups.ParallelRaceGroup
 import dev.nextftc.core.commands.groups.SequentialGroup
 import dev.nextftc.core.commands.utility.InstantCommand
 import dev.nextftc.core.components.BindingsComponent
@@ -17,10 +16,12 @@ import dev.nextftc.hardware.impl.MotorEx
 import org.firstinspires.ftc.teamcode.next.kotlin.subsystems.Intake
 import org.firstinspires.ftc.teamcode.next.kotlin.subsystems.Outtake
 import kotlin.time.Duration.Companion.seconds
+import com.pedropathing.geometry.BezierLine
+import com.pedropathing.geometry.Pose
 
 @Autonomous
-class Auto3ball : NextFTCOpMode() {
-    init{
+class AutoBackwardShoot : NextFTCOpMode() {
+    init {
         addComponents(
             SubsystemComponent(Outtake, Intake),
             PedroComponent(Constants::createFollower),
@@ -28,65 +29,64 @@ class Auto3ball : NextFTCOpMode() {
             BindingsComponent
         )
     }
+
     var fl = MotorEx("fl")
     var fR = MotorEx("fr")
     var bR = MotorEx("br")
     var bL = MotorEx("bl")
-    val drive = MotorGroup(fl,fR,bR,bL)
+    val drive = MotorGroup(fl, fR, bR, bL)
 
-    private val prepToShootFT: Command
-        get() = SequentialGroup(
-            Intake.reverseIntake,
-            Delay(1.seconds),
-            ParallelGroup(
-                Outtake.flywheelOn,
-                Delay(1.seconds),
-                Intake.runIntake
-            ),
-            Delay(0.5.seconds),
-            ParallelGroup(
-                Intake.stopIntake,
-                Outtake.flywheelOff
+    private val START_POSE = Pose(20.0, 123.0, Math.toRadians(144.0))
+    private val BACKWARD_POSE = Pose(38.5, 104.0, Math.toRadians(144.0))  // Move backward (increase Y)
+    private val END_POSE = Pose(54.0, 60.0, Math.toRadians(180.0))
 
-            )
-        )
-    private val preIntakeLine : Command
+    private val shootSequence: Command
         get() = SequentialGroup(
+            // Turn on flywheel
+            Outtake.flywheelOn,
+            // Wait for flywheel to spin up
+            Delay(1.5.seconds),
+            // Run intake to feed balls into shooter
             Intake.runIntake,
+            // Shoot for 3 seconds
             Delay(3.seconds),
-            Intake.reverseIntake,
-            Delay(1.seconds),
-            ParallelGroup(
-                Intake.stopIntake,
-                Outtake.flywheelOn
-            )
-        )
-    private val Shoot : Command
-        get()= SequentialGroup(
-            Intake.runIntake,
-            Delay(5.seconds),
+            // Stop everything
             ParallelGroup(
                 Intake.stopIntake,
                 Outtake.flywheelOff
             )
         )
+
+    private val driveBackwardPath: Command
+        get() = InstantCommand {
+            val path = PedroComponent.follower
+                .pathBuilder()
+                .addPath(BezierLine(START_POSE, BACKWARD_POSE))
+                .setLinearHeadingInterpolation(START_POSE.heading, BACKWARD_POSE.heading)
+                .build()
+            PedroComponent.follower.followPath(path)
+        }
+
+    private val moveRightPath: Command
+        get() = InstantCommand {
+            val path = PedroComponent.follower
+                .pathBuilder()
+                .addPath(BezierLine(BACKWARD_POSE, END_POSE))
+                .setLinearHeadingInterpolation(BACKWARD_POSE.heading, END_POSE.heading)
+                .build()
+            PedroComponent.follower.followPath(path)
+        }
 
     override fun onStartButtonPressed() {
         SequentialGroup(
-        InstantCommand {
-            drive.power = 0.5
-        },
-        Delay(0.1.seconds),
-        InstantCommand {
-            drive.power = 0.0
-        },
-            prepToShootFT
-
-
-
-
-        )
+            // Drive backwards using Pedro for 1 second
+            driveBackwardPath,
+            Delay(1.seconds),
+            // Execute shoot sequence
+            shootSequence,
+            // Move right for 5 seconds
+            moveRightPath,
+            Delay(5.seconds)
+        ).schedule()
     }
-
-
 }
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Auto3ballJava.java
@@ -0,0 +1,84 @@
+package org.firstinspires.ftc.teamcode.pedroPathing;
+
+import com.bylazar.configurables.annotations.Configurable;
+import com.bylazar.telemetry.PanelsTelemetry;
+import com.bylazar.telemetry.TelemetryManager;
+import com.pedropathing.follower.Follower;
+import com.pedropathing.geometry.BezierLine;
+import com.pedropathing.geometry.Pose;
+import com.pedropathing.paths.PathChain;
+import com.pedropathing.util.Timer;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+import org.firstinspires.ftc.teamcode.pedroPathing.Constants;
+
+@Autonomous(name = "SimpleMoveRight", group = "Autonomous")
+@Configurable
+public class Auto3ballJava extends OpMode {
+
+    protected Follower follower;
+    protected Timer moveTimer;
+    int pathState = 0;
+
+    private static final Pose START_POSE = new Pose(21.084, 122.018, Math.toRadians(180));
+    private static final Pose END_POSE = new Pose(21.084, 60.0, Math.toRadians(180)); // Move right (decrease Y)
+
+    private TelemetryManager panelsTelemetry;
+    private PathChain moveRightPath;
+
+    @Override
+    public void init() {
+        moveTimer = new Timer();
+        panelsTelemetry = PanelsTelemetry.INSTANCE.getTelemetry();
+
+        follower = Constants.createFollower(hardwareMap);
+        follower.setStartingPose(START_POSE);
+
+        // Build the path to move right
+        moveRightPath = follower
+                .pathBuilder()
+                .addPath(new BezierLine(START_POSE, END_POSE))
+                .setLinearHeadingInterpolation(START_POSE.getHeading(), END_POSE.getHeading())
+                .build();
+
+        panelsTelemetry.debug("Status", "Initialized - Ready to move right");
+        panelsTelemetry.update(telemetry);
+    }
+
+    @Override
+    public void start() {
+        moveTimer.resetTimer();
+    }
+
+    @Override
+    public void loop() {
+        follower.update();
+
+        switch (pathState) {
+            case 0:
+                // Start moving right
+                follower.followPath(moveRightPath);
+                pathState++;
+                break;
+            case 1:
+                // Keep moving for 5 seconds or until path completes
+                if (moveTimer.getElapsedTimeSeconds() >= 5.0) {
+                    pathState++;
+                }
+                break;
+            case 2:
+                // Stop
+                break;
+        }
+
+        panelsTelemetry.debug("Path State", pathState);
+        panelsTelemetry.debug("Time Elapsed", moveTimer.getElapsedTimeSeconds());
+        panelsTelemetry.debug("X", follower.getPose().getX());
+        panelsTelemetry.debug("Y", follower.getPose().getY());
+        panelsTelemetry.debug("Heading", Math.toDegrees(follower.getPose().getHeading()));
+        panelsTelemetry.update(telemetry);
+    }
+}
